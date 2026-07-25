610faf1664988323a5e93b3a3d0256aa13408001
stefanlv <matei.04.yahoo@gmail.com>
2026-07-25 21:58:45 +0300

    changed how the organism works and added a second hero to the home page

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,38 @@ import { Header } from './components/organisms/header/header';
 import { Hero } from './components/organisms/hero/hero'; 
 import { Footer } from './components/organisms/footer/footer'; 
 
-function App() {
+
+function App(){
+
+
+  const carouselSlides = [
+    {
+      title: "Fresh Vegetables Big discount",
+      subtitle: "Save up to 50% off on your first order",
+      bgImage: "src/assets/header/hero1.png",
+    },
+    {
+      title: "Don’t miss our daily fresh products",
+      subtitle: "Sign up for the daily newsletter",
+      bgImage: "src/assets/header/hero2.png",
+    },
+  ];
+
+  const singleSlide = [
+    {
+      title: "Stay home & get your daily needs from our shop",
+      subtitle: "Start Your Daily Shopping with Nest Mart",
+      bgImage: "src/assets/header/hero-second.png",
+    }
+  ];
+
   return (
     <div>
       <Header />
       
       <main>
-        <Hero /> 
+        <Hero slides={carouselSlides} /> 
+        <Hero slides={singleSlide} /> 
         <Footer />
       </main>
     </div>
